80429d679373971ebdf2781c31e21cc3ae88fcfb
Eldar Muminov <emuminov@z4r2p4.42lyon.fr>
2024-07-31 19:33:09 +0200

    feat: added vector functions, started dda line drawing algo

--- src/raycasting/_test.c
+++ src/raycasting/_test.c
@@ -6,7 +6,7 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 16:12:16 by emuminov          #+#    #+#             */
-/*   Updated: 2024/07/31 18:36:43 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/07/31 19:32:36 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,19 +27,26 @@ const int map[10][10] = {
 	{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
 };
 
-typedef struct	s_img {
+typedef struct	s_img
+{
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
-	int		line_length;
+	int		line_len;
 	int		endian;
 }				t_img;
 
-void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
+typedef struct	s_vector
+{
+	double	x;
+	double	y;
+}				t_vector;
+
+void	paint_img_pixel(t_img *img, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+	dst = img->addr + (y * img->line_len + x * (img->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
 }
 
@@ -48,16 +55,42 @@ void paint_tile(void *mlx, void *win, int x, int y, int color)
 	t_img	img;
 
 	img.img = mlx_new_image(mlx, TILE_SIZE, TILE_SIZE);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_len,
 			&img.endian);
 	for (int y = 0; y < TILE_SIZE; y++) {
 		for (int x = 0; x < TILE_SIZE; x++) {
-			my_mlx_pixel_put(&img, x, y, color);
+			paint_img_pixel(&img, x, y, color);
 		}
 	}
 
 	mlx_put_image_to_window(mlx, win, img.img, x * TILE_SIZE, y * TILE_SIZE);
 	mlx_destroy_image(mlx, img.img);
+}
+
+t_vector vector_sub(t_vector v1, t_vector v2)
+{
+	return (t_vector){.x=v1.x - v2.x, .y=v1.y - v2.y};
+}
+
+t_vector vector_abs(t_vector v)
+{
+	t_vector res;
+
+	if (v.x < 0)
+		res.x = -v.x;
+	if (v.y < 0)
+		res.y = -v.y;
+	return res;
+}
+
+void draw_line(t_vector start, t_vector end)
+{
+	t_vector	delta;
+	double		slope;
+
+	delta = vector_abs(vector_sub(end, start));
+	slope = delta.y / delta.x;
+	
 }
 
 int main()
