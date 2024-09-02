e6acabacaa2e402a1db18dfb2464723aa2a0a097
Elio Andre <eandre@z2r4p4.42lyon.fr>
2024-09-02 16:30:44 +0200

    git: merge fixes

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 NAME = cub3D
 
 CC = cc
-CFLAGS = -Wall -Wextra -Werror -g3
+CFLAGS = -Wall -Wextra -Werror -O2
 XFLAGS = -lXext -lX11
 
 OBJS_DIR = .obj/
--- src/game_logic/_exit_game.c
+++ src/game_logic/_exit_game.c
@@ -6,25 +6,25 @@
 /*   By: eandre <eandre@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 15:51:03 by emuminov          #+#    #+#             */
-/*   Updated: 2024/09/02 16:27:31 by eandre           ###   ########.fr       */
+/*   Updated: 2024/09/02 16:28:37 by eandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
 #include "../minilibx-linux/mlx.h"
 #include <stdlib.h>
-void	destroy_animation_textures(t_game *g, t_img *textures, int size);
+void	destroy_textures_array(t_game *g, t_img *textures, int size);
 
 int	exit_game(t_game *g)
 {
 	mlx_destroy_image(g->mlx, g->frame.img);
-	destroy_animation_textures(g, g->texture, 4);
-	destroy_animation_textures(g, g->idle_textures, IDLE_FRAMES);
-	destroy_animation_textures(g, g->idle_flower_textures, IDLE_FLOWER_FRAMES);
-	destroy_animation_textures(g, g->walk_textures, WALK_FRAMES);
-	destroy_animation_textures(g, g->walk_flower_textures, WALK_FLOWER_FRAMES);
-	destroy_animation_textures(g, g->flower_in_textures, FLOWER_IN_FRAMES);
-	destroy_animation_textures(g, g->flower_out_textures, FLOWER_OUT_FRAMES);
+	destroy_textures_array(g, g->wall_textures, 4);
+	destroy_textures_array(g, g->idle_textures, IDLE_FRAMES);
+	destroy_textures_array(g, g->idle_flower_textures, IDLE_FLOWER_FRAMES);
+	destroy_textures_array(g, g->walk_textures, WALK_FRAMES);
+	destroy_textures_array(g, g->walk_flower_textures, WALK_FLOWER_FRAMES);
+	destroy_textures_array(g, g->flower_in_textures, FLOWER_IN_FRAMES);
+	destroy_textures_array(g, g->flower_out_textures, FLOWER_OUT_FRAMES);
 	mlx_destroy_window(g->mlx, g->win);
 	mlx_destroy_display(g->mlx);
 	free(g->mlx);
